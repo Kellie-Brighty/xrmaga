6beae47c4b2d6d113c2deec8ca70bf33adc2fbe4
Kelly Owoju <kelliebrighty@gmail.com>
2025-02-07 18:44:44 +0100

    made updates

--- src/components/Tokenomics.tsx
+++ src/components/Tokenomics.tsx
@@ -10,7 +10,7 @@ const Tokenomics: React.FC = () => {
     { name: "Liquidity Pool", value: 70, color: "#FF0000" },
     // { name: "Community", value: 10, color: "#FFFFFF" },
     { name: "Development", value: 10, color: "#0000FF" },
-    { name: "Marketing", value: 10, color: "#FF4444" },
+    { name: "Marketing", value: 20, color: "#FF4444" },
   ];
 
   const copyToClipboard = () => {
